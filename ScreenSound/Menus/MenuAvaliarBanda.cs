55867db0a2499a8f6bd25c35f981b16d1695ad27
Lucas <lukkascomics@gmail.com>
2023-07-04 09:22:46 -0300

    Changes

---
+++ ScreenSound/Menus/MenuAvaliarBanda.cs
@@ -0,0 +1,48 @@
+﻿using ScreenSound.Models;
+
+namespace ScreenSound.Menus;
+
+internal class MenuAvaliarBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        {
+            base.Executar(bandasRegistradas);
+            ExibirTituloDaOpcao("Avaliar banda");
+            Console.Write("Digite o nome da banda que deseja avaliar: ");
+            string nomeDaBanda = Console.ReadLine()!;
+            bool bandaEncontrada = false;
+
+            foreach (var bandaRegistrada in bandasRegistradas)
+            {
+                if (bandaRegistrada.Key.Equals(nomeDaBanda, StringComparison.OrdinalIgnoreCase))
+                {
+                    Banda banda = bandaRegistrada.Value;
+                    Console.Write($"Qual a nota que a banda {bandaRegistrada.Key} merece: ");
+                    Avaliacao nota = Avaliacao.AvaliacaoParse(Console.ReadLine()!);
+                    banda.AdicionarNota(nota);
+                    while (nota.Nota > 10 || nota.Nota < 1)
+                    {
+                        Console.WriteLine("Nota inválida. A nota precisa estar entre 1 a 10...");
+                        Console.Write("Digite a nota novamente: ");
+                        nota = Avaliacao.AvaliacaoParse(Console.ReadLine()!);
+                    }
+                    Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para a banda {bandaRegistrada.Key}");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    bandaEncontrada = true;
+                    break;
+                }
+            }
+
+            if (!bandaEncontrada)
+            {
+                Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
+                Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+    }
+}
---
+++ ScreenSound/Menus/RegistrarAlbum.cs
@@ -0,0 +1,43 @@
+﻿using ScreenSound.Models;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarAlbum : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro de álbuns");
+        Console.Write("Digite a banda cujo álbum deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+
+        if (nomeDaBanda.ToUpper() == "X")
+        {
+            Console.Clear();
+            return;
+        }
+
+        while (!bandasRegistradas.ContainsKey(nomeDaBanda))
+        {
+            Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
+            Console.Write("Digite o nome da banda novamente ");
+            Console.WriteLine("ou digite X para voltar ao menu.");
+            nomeDaBanda = Console.ReadLine()!;
+
+            if (nomeDaBanda.ToUpper() == "X")
+            {
+                Console.Clear();
+                return;
+            }
+        }
+
+        Console.Write("Agora digite o título do álbum: ");
+        string tituloAlbum = Console.ReadLine()!;
+        Banda banda = bandasRegistradas[nomeDaBanda];
+        banda.AdicionarAlbum(new Album(tituloAlbum));
+
+        Console.WriteLine($"O álbum {tituloAlbum} de {nomeDaBanda} foi registrado com sucesso!");
+        Thread.Sleep(2000);
+        Console.Clear();
+    }
+}
